beb4d81e007a551f1aa9a5eadd531daccee84580
chunswu <chunswu@gmail.com>
2021-01-11 09:00:25 -0700

    added relu like function and cleaned up code

--- src/spark_gb.py
+++ src/spark_gb.py
@@ -7,62 +7,21 @@ from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import VectorIndexer
 from pyspark.ml.regression import GBTRegressor
 from pyspark.ml.evaluation import RegressionEvaluator
+import pyspark.sql.functions as F
 import sklearn.metrics
 import numpy as np
 
-def get_dummy(df,indexCol,categoricalCols,continuousCols,labelCol,dropLast=False):
-    '''Get dummy variables and concat with continuous variables for ml modeling
+def transData(data):
+    '''Combines all columns of Spark dataframe into a list columns
 
     Parameters
     ----------
-    df: the dataframe
-    categoricalCols: the name list of the categorical data
-    continuousCols:  the name list of the numerical data
-    labelCol:  the name of label column
-    dropLast:  the flag of drop last column
+    df: dataframe in sparks
     
     Returns
     -------
-    features matrix
+    returns a dataframe
     '''
-    indexers = [StringIndexer(inputCol=c, outputCol='{0}_indexed'.format(c))
-                 for c in categoricalCols]
-
-    # default setting: dropLast=True
-    encoders = [OneHotEncoder(inputCol=indexer.getOutputCol(),
-                outputCol='{0}_encoded'.format(indexer.getOutputCol()),dropLast=dropLast)
-                for indexer in indexers]
-
-    assembler = VectorAssembler(inputCols=[encoder.getOutputCol() for encoder in encoders]
-                                + continuousCols, outputCol='features')
-
-    pipeline = Pipeline(stages=indexers + encoders + [assembler])
-
-    model=pipeline.fit(df)
-    data = model.transform(df)
-
-    if indexCol and labelCol:
-        # for supervised learning
-        data = data.withColumn('label',col(labelCol))
-        return data.select(indexCol,'features','label')
-    elif not indexCol and labelCol:
-        # for supervised learning
-        data = data.withColumn('label',col(labelCol))
-        return data.select('features','label')
-    elif indexCol and not labelCol:
-        # for unsupervised learning
-        return data.select(indexCol,'features')
-    elif not indexCol and not labelCol:
-        # for unsupervised learning
-        return data.select('features')
-
-# convert the data to dense vector
-#def transData(row):
-#    return Row(label=row['Sales'],
-#               features=Vectors.dense([row['TV'],
-#                                       row['Radio'],
-#                                       row['Newspaper']]))
-def transData(data):
     return data.rdd.map(lambda r: [Vectors.dense(r[:-1]),r[-1]]).toDF(['features','label'])
 
 
@@ -74,21 +33,19 @@ if __name__ == '__main__':
         )
     sc = spark.sparkContext
     df = spark.read.parquet('../model/data.parquet')
-    # print('TYPE: ', type(df))
+
+    df = df.drop('api')
     df.show()
     transformed= transData(df)
-    # transformed.show(5, False)
+
     featureIndexer = VectorIndexer(inputCol='features', 
                                    outputCol='indexedFeatures',
                                    maxCategories=4).fit(transformed)
 
     data = featureIndexer.transform(transformed) 
-    # data.show(5, False)
 
     (train_data, test_data) = data.randomSplit([0.7, 0.3], seed=88) 
-    r2_lst = []
-    # range(0.01, 1.0, 0.01)
-    # for tree in range(35, 105, 5): # np.linspace(0.05, 1, 20):
+
     gbt = GBTRegressor(featuresCol='indexedFeatures',
                         featureSubsetStrategy='auto',
                         stepSize=0.05,
@@ -106,10 +63,9 @@ if __name__ == '__main__':
     model = pipeline.fit(train_data)
 
     predictions = model.transform(test_data)
-
-    # Select example rows to display.
     predictions.select('features', 'label', 'prediction').show(100, False)
-
+    predictions = predictions.withColumn("prediction", F.when(F.col("prediction") > 0, F.col("prediction")).otherwise(0))
+    predictions.describe().show()
     evaluator = RegressionEvaluator(labelCol='label', 
                                     predictionCol='prediction', 
                                     metricName='rmse')
@@ -120,14 +76,14 @@ if __name__ == '__main__':
     y_true = predictions.select('label').toPandas()
     y_pred = predictions.select('prediction').toPandas()
     r2_score = sklearn.metrics.r2_score(y_true, y_pred)
-    # r2_lst.append([tree, r2_score, rmse])
     print('r2_score: {:4.3f}'.format(r2_score))
 
     print('FEATURES IMPORTANCES: ', model.stages[-1].featureImportances)
-
     model.stages[-1].trees
 
-    # print(r2_lst)
-
     rfModel = model.stages[1]
-    print(rfModel)  # summary only
+    print(rfModel)
+    va = model.stages[-2]
+    tree = model.stages[-1]
+    display(tree)
+    print(list(zip(va.getOutputCol(), tree.featureImportances)))
